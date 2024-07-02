16bda68f7490efe94057392be96622afd06b92f2
Saurabh Tripathi <saurabh.tripathi@xaigi.tech>
2024-07-02 21:49:34 +0530

    Added shimmer-effect and integrated swiggy live api.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,31 +1,41 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData"; //names exports are always gonna import like this
 import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 //It's not recommended to use index as key but uou can use it, if necessary
 //not using keys (not accceptable) <<<< index <<<<<< unique id(best)
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState([])
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchSwiggyData();
-  },[])
+  }, []);
 
-  const fetchSwiggyData = async () =>{
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  const fetchSwiggyData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
     const jsonData = await data.json();
-    console.log("json",jsonData);
-    setListOfRestaurants(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-  }
-  
-  return (
+    console.log("json", jsonData);
+    setListOfRestaurants(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
-      {/* <div className="search">Search</div> */}
       <div className="filter">
+        <div className="search">
+          <input type="text" className="search-box"/>
+          <button>Search</button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
-            const filteredList = listOfRestaurants.filter((res) => res?.info?.avgRating > 4.3);
+            const filteredList = listOfRestaurants.filter(
+              (res) => res?.info?.avgRating > 4.3
+            );
             setListOfRestaurants(filteredList);
           }}
         >
--- src/components/Header.js
+++ src/components/Header.js
@@ -15,6 +15,7 @@ const Header = () => {
             <li>About Us</li>
             <li>Contact Us</li>
             <li>Cart</li>
+            <button className="login">Login</button>
           </ul>
         </div>
       </div>
