30c6510f433904493a433d6a095b75bf6a3fffab
Saurabh Tripathi <saurabh.tripathi@xaigi.tech>
2024-09-30 17:47:35 +0530

    Added custom hook for online/offline, used lazy loading and configured tailwindcss in the app.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import { LOGO_URL } from "../utils/constants";
 import { useNavigate } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
   const navigate = useNavigate();
-
+  const onlineStatus = useOnlineStatus();
     return (
       <div className="header">
         <div>
@@ -13,10 +14,12 @@ const Header = () => {
           />
         </div>
         <div className="nav-items">
-          <ul>
+          <ul style={{cursor: "pointer"}}>
+            <li>Online Status:{onlineStatus ? "🟢" : "🔴"}</li>
             <li onClick={() => navigate("/")}>Home</li>
             <li onClick={() => navigate("/about")}>About Us</li>
             <li onClick={() => navigate("/contact-us")}>Contact Us</li>
+            <li onClick={() => navigate("/grocery")}>Grocery</li>
             <li onClick={() => navigate("/cart")}>Cart</li>
             <button className="login">Login</button>
           </ul>
--- src/utils/useOnlineStatus.js
+++ src/utils/useOnlineStatus.js
@@ -14,6 +14,7 @@ const useOnlineStatus = () => {
     });
   }, []);
 
+  //boolean value
   return status;
 };
 
